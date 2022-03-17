0435addc8f69d27e804756d2f024add44d925615
Murali090 <97532417+Murali090@users.noreply.github.com>
2022-03-17 12:36:23 +0530

    Add files via upload

---
+++ conversion data types.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=10
+print(a)
+print(type(a))
+b=str(a)
+print(b)
+print(type(b))
+c=float(a)
+print(c)
+print(type(c))
+d=bool(a)
+print(d)
+print(type(d))
+e=complex(a)
+print(e)
+print(type(e))
+
+
+
+# In[5]:
+
+
+a='10'
+print(type(a))
+b=int(a)
+print(b)    #cant convert alphabets into  integers,instead give digits from 0-9.
+print(type(b))
+c=float(a)
+print(c)
+print(type(c))
+d=bool(a)
+print(d)
+print(type(d))
+e=complex(a)
+print(e)
+print(type(e))
+
+
+# # converting float into other data types
+# 
+
+# In[10]:
+
+
+a=20.2
+print(type(a))
+b=str(a)
+print(b)
+print(type(b))
+c=int(a)
+print(c)
+print(type(c))
+d=bool(a)
+print(d)
+print(type(d))
+e=complex(a)
+print(e)
+print(type(e))
+
+
+# # converting complex into other data types
+
+# In[15]:
+
+
+a=2+i3      #complex cannot be converted 
+print(type(a))
+b=int(a)
+print(b)
+print(type(b))
+c=str(a)
+print(c)
+print(type(c))
+d=float(a)
+print(d)
+print(type(d))
+e=bool(a)
+print(e)
+print(type(e))
+
+
+# In[ ]:
+
+
+
+
